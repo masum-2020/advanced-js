b185da60f4f57246845bed044786b5ac8b034cfa
masum-2020 <almsm014@gmail.com>
2021-08-29 10:57:28 +0600

    null vs undefined

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,20 +1,20 @@
-//let pakhi;
-// console.log(pakhi);
+let pakhi;
+console.log(pakhi);
 
-// function add(num1, num2){
-// console.log(num1 + num2);
-// return ;
-// }
+function add(num1, num2){
+console.log(num1 + num2);
+return ;
+}
 
-// function add(num1, num2);{
-//     console.log(num1, num2);
-// }
+function add(num1, num2);{
+    console.log(num1, num2);
+}
 
-// const premik = {name:"Saddam khan", phone:48744};
-// console.log(premik.gf);
+const premik = {name:"Saddam khan", phone:48744};
+console.log(premik.gf);
 
-// console.log(fun);
-// let fun = undefined;
+console.log(fun);
+let fun = undefined;
 
 let ages = [2, 5, 9]
 console.log(ages[22]);
