dea96882b608f77fe6608bf152ff7d6348e850be
masum-2020 <almsm014@gmail.com>
2021-08-29 09:56:41 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,24 @@
+//Falsy:
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// '0', ' ', [], {}
+let name = {};
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+// const name = "Salman";
+
+// if(name.length > 0){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
